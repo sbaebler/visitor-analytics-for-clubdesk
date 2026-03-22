4ae46bd36bcec26f1e6d7474783caf495574bfbb
Serafin Bäbler <serafin.baebler@gmail.com>
2026-03-22 14:20:03 +0100

    Temp: enable error display on dashboard

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 declare(strict_types=1);
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
 
 require_once __DIR__ . '/../src/Auth.php';
 require_once __DIR__ . '/../src/Database.php';
